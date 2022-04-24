d61a28eed26e49d8365caefb17704be393d1075a
Danil Shabanov <shabanov1903@gmail.com>
2022-04-24 21:59:16 +0300

    Correct carousel on main tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainModule } from './pages/main/main.module';
-import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,9 +12,7 @@ import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MainModule,
-    NgbModule,
-    NgbNavModule
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
