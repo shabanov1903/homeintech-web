49059b798e1e40cb5987b9918aa36f7da5dd5bc7
Danil Shabanov <shabanov1903@gmail.com>
2022-04-24 20:38:53 +0300

    Delete bootstrap from project

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { CardModule } from 'src/app/components/card/card.module';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import {CarouselModule} from 'primeng/carousel';
 
 @NgModule({
@@ -12,7 +11,6 @@ import {CarouselModule} from 'primeng/carousel';
   imports: [
     CommonModule,
     CardModule,
-    NgbCarouselModule,
     CarouselModule
   ]
 })
