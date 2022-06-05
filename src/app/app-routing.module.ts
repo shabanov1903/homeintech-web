8bc29dfb8e8322344b8d4f1577580ae0070b703f
Danil Shabanov <shabanov1903@gmail.com>
2022-06-05 23:03:09 +0300

    Add serv and map components and correct routing for new elements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { MapComponent } from './pages/map/map.component';
+import { ServComponent } from './pages/serv/serv.component';
 import { TechComponent } from './pages/tech/tech.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
   { path: 'tech/:id', component: TechComponent },
+  { path: 'serv/:id', component: ServComponent },
+  { path: 'map/:id', component: MapComponent },
   { path: '',   redirectTo: '/main', pathMatch: 'full' }
 ];
 
---
+++ src/app/pages/serv/serv.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServComponent } from './serv.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServModule { }
